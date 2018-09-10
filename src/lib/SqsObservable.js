04a4d140e4c163e40c527ebef9c6acedbaa815e2
Pappa <dr.pappa@gmail.com>
2018-09-10 18:41:20 +0100

    SqsObservable

--- src/lib/SqsObservable.js
+++ src/lib/SqsObservable.js
@@ -1,7 +1,31 @@
-import { Observable } from "rxjs";
+import { Observable, Subscriber } from "rxjs";
 
 export default class SqsObservable {
     constructor(options, queue) {
+        this.options = options;
+        this.queue = queue;
+        
+        return Observable.create((observer) => {
+            const subscriber = new Subscriber(openObserver);
+            
+            const onOpen = (event) => {
+                subscriber.next(event);
+                subscriber.complete();
+            };
+        
+            const onMessage = (message) => {
+                observer.next(JSON.parse(message));
+            };
+        
+            const onError = (error) => {
+                observer.error(error); // TODO: observable contract
+                observer.complete();
+            };
+
+			return () => {
+				// unsubscribe
+			};
+		});
     }
   
     static create = (options, queue) => new SqsObservable(options, queue);
