bee39f3fbf96d5686c0b3bdc65ff7543b14e6faf
Pappa <dr.pappa@gmail.com>
2018-09-11 11:36:43 +0100

    Removing webpack

--- src/lib/Squirty.js
+++ src/lib/Squirty.js
@@ -6,9 +6,13 @@ import SqsObservable from "./SqsObservable";
 export default class Squirty {
   constructor(options, queue) {
     if (!options.QueueUrl) {
-      throw "QueueUrl is a required option property";
+      throw "options.QueueUrl is required";
     }
-    const sqs = queue || new SQS({ region: options.Region || process.env.AWS_REGION || "eu-west-1"});
+    const sqs =
+      queue ||
+      new SQS({
+        region: options.Region || process.env.AWS_REGION || "eu-west-1"
+      });
     const params = Object.assign({}, defaultParams, options);
 
     return SqsObservable.create(params, sqs);
