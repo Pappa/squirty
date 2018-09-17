f5a324bdce98b37aab548dd44e061171590a7647
Pappa <dr.pappa@gmail.com>
2018-09-17 18:27:05 +0100

    SqsObservable

--- src/lib/SqsObservable.js
+++ src/lib/SqsObservable.js
@@ -1,29 +1,40 @@
-import { Observable, Subscriber } from "rxjs";
+import { Observable } from "rxjs";
 
 class SqsObservable {
     constructor(options, queue) {
         this.options = options;
         this.queue = queue;
+        this.shouldPoll = false;
         
         return Observable.create((observer) => {
-            const subscriber = new Subscriber(openObserver);
-            
-            const onOpen = (event) => {
-                subscriber.next(event);
-                subscriber.complete();
+        
+            const onMessage = (data) => {
+                observer.next(data);
             };
         
-            const onMessage = (message) => {
-                observer.next(JSON.parse(message));
-            };
-        
-            const onError = (error) => {
-                observer.error(error); // TODO: observable contract
+            const onError = (e) => {
+                this.shouldPoll = false;
+                observer.error(e); // TODO: observable contract
                 observer.complete();
             };
 
+            const pollForMessages = () => {
+                this.queue.receiveMessage(this.options, (e, data) => {
+                    if (e) {
+                        onError(e);
+                    } else {
+                        onMessage(data);
+                        if (this.shouldPoll) {
+                            pollForMessages();
+                        }
+                    }
+                  });
+            };
+
+            pollForMessages();
+
 			return () => {
-				// unsubscribe
+                this.shouldPoll = false;
 			};
 		});
     }
